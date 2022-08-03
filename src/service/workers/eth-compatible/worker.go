52d2cf4f23bce6c20eed14aa931178ecd4f1a61a
arout2 <aditya.rout@latoken.com>
2022-08-03 15:52:31 +0530

    changed height settings

--- src/service/workers/eth-compatible/worker.go
+++ src/service/workers/eth-compatible/worker.go
@@ -268,8 +268,10 @@ func (w *Erc20Worker) GetFetchInterval() time.Duration {
 // getLogs ...
 func (w *Erc20Worker) getLogs(curHeight, nextHeight int64) ([]*storage.TxLog, error) {
 	//	topics := [][]common.Hash{{DepositEventHash, ProposalEventHash, ProposalVoteHash}}
-	if curHeight == 0 || nextHeight-curHeight >= 3500 {
+	if curHeight == 0 {
 		curHeight = nextHeight - 1
+	} else if nextHeight-curHeight >= 3500 {
+		nextHeight = curHeight + 1
 	}
 	logs, err := w.client.FilterLogs(context.Background(), ethereum.FilterQuery{
 		// BlockHash: &blockHash,
